8f45cb3d1a8f64f82964115b5acaca149a35ad59
Aleksandr Limarev <limarev.sanya@gmail.com>
2022-11-14 15:01:47 +0100

    Initial Commit

---
+++ MeetApp/MeetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetAppApp.swift
+//  MeetApp
+//
+//  Created by Александр Лимарев on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
